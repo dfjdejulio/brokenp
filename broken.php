59be0f53c61a032956b8921fa55408e750362b1c
Doug DeJulio <ddj@cmu.edu>
2014-11-17 10:09:36 -0500

    A little more logic.

--- broken.php
+++ broken.php
@@ -7,12 +7,19 @@
 <h1>brokenp?</h1>
 <hr/>
 <?php
-$get = $_GET["url"];
-if ($get) { ?>
-Stuff goes here.
-<hr/>
-<?php } ?>
-<form>
+$my_url = $_GET["url"];
+if ($my_url) { 
+
+$ch = curl_init($my_url);
+curl_setopt($ch, CURLOPT_FAILONERROR, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+curl_setopt($ch, CURLOPT_FRESH_CONNECT, TRUE);
+$result = curl_exec($ch);
+
+echo "Result: $result";
+
+?><hr/><?php } ?>
+<form type="get">
 URL:
 <input type="text" name="url" value="<?= $get ?>" style="width:100%;"/>
 <input type="submit"/>
