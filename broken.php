4babb264fe4b7f1c7b6e86a7ee263ab5c01140c9
Doug DeJulio <ddj@cmu.edu>
2014-11-17 09:29:22 -0500

    Early content, not yet functional.

---
+++ broken.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>brokenp?</title>
+</head>
+<body>
+<h1>brokenp?</h1>
+<hr/>
+<?php
+?>
+$get = $_GET["url"];
+<form>
+URL:
+<input type="text" name="url" value="$get" style="width:100%;"/>
+<input type="submit"/>
+</form>
+</body>
+</html>
